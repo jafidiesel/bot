685fc8475482e3b884bd025a51cb3273fd3a5743
jafidiesel <jafibravin@outlook.com>
2023-07-20 17:56:53 -0300

    Added dot-env

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,7 +14,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('<token>').build()
+    application = ApplicationBuilder().token(config['TELEGRAM_TOKEN']).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
