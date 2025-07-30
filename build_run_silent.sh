d0b44255a7e8811497ed994db39b6dec5dbc4cf9
jafidiesel <jafibravin@outlook.com>
2025-07-30 19:17:57 -0300

    updates

--- build_run_silent.sh
+++ build_run_silent.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-cd /home/pi/git/bot
+# Definir la ruta del proyecto
+PROJECT_DIR="/home/pi/git/bot"
 
+# Cambiar al directorio del proyecto
+cd "$PROJECT_DIR"
+
+# Limpiar instalación anterior del virtual environment
 rm -rf bin
 rm -rf include
 rm -rf lib
-rm lib64
-rm pyvenv.cfg
+rm -f lib64
+rm -f pyvenv.cfg
 
+# Crear nuevo virtual environment
 python3 -m venv .
 
+# Activar virtual environment
 source bin/activate
 
-pip3 install python-telegram-bot --upgrade python-dotenv requests
+# Actualizar pip
+pip3 install --upgrade pip
 
+# Instalar dependencias desde requirements.txt
+pip3 install -r requirements.txt
+
+# Ejecutar el bot
 python3 bot.py
