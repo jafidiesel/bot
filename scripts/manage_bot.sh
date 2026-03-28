2cef1e3b2297aa896b5f6cf892b1ba47d0f04411
Jafi <jafidiesel@users.noreply.github.com>
2026-03-28 12:38:55 -0300

    Actualizar manage_bot.sh

--- scripts/manage_bot.sh
+++ scripts/manage_bot.sh
@@ -16,9 +16,9 @@ PYTHON=”${VENV_DIR}/bin/python3”
 
 # Colores para output
 
-RED=’\033[0;31m’
-GREEN=’\033[0;32m’
-YELLOW=’\033[1;33m’
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 NC=’\033[0m’ # No Color
 
 print_ok()   { echo -e “${GREEN}[OK]${NC} $1”; }
